d5c69284d39f87330131b336045df27ba509b452
zwpdbh <hyperion_z@outlook.com>
2023-03-04 21:52:15 +0800

    <plot could works, > use fsi from terminal with argument

--- src/Visualization/TimeSeries01.fsx
+++ src/Visualization/TimeSeries01.fsx
@@ -1,11 +1,11 @@
 #r "nuget: Plotly.NET"
-#r "nuget: Plotly.NET.LayoutObjects"
 #r "nuget: FSharp.Data"
-
 
 open Plotly.NET
 open Plotly.NET.LayoutObjects
 open FSharp.Data
+open System
+
 
 let data =
     CsvFile.Load("https://raw.githubusercontent.com/plotly/datasets/master/stockdata.csv")
@@ -23,12 +23,30 @@ let getChart (stock: string) =
 
     Chart.Line(x = x, y = y, Name = stock)
 
+let demo01 () = 
+    [ 
+        getChart "AAPL"
+        getChart "SBUX"
+        getChart "IBM"
+        getChart "MSFT" ]
+    |> Chart.combine
+    |> Chart.withXAxis (LinearAxis.init (DTick = "M1", TickFormat = "%b\n%Y", TickLabelMode=StyleParam.TickLabelMode.Period))
+    |> Chart.withTitle("custom tick labels with TickLabelMode='Period'")
+    |> Chart.show
 
-[ getChart "AAPL"
-  getChart "SBUX"
-  getChart "IBM"
-  getChart "MSFT" ]
-|> Chart.combine
-|> Chart.withXAxis (LinearAxis.init (DTick = "M1", TickFormat = "%b\n%Y", TickLabelMode=StyleParam.TickLabelMode.Period))
-|> Chart.withTitle("custom tick labels with TickLabelMode='Period'")
-|> Chart.show
+let demo02 () = 
+    let ts = [0. .. 0.1 .. 10.]
+    let ys = ts |> Seq.map (Math.Sin)
+    Chart.Scatter(ts, ys, mode=StyleParam.Mode.Markers)
+    |> Chart.show
+
+
+// run it from command-line with 
+// dotnet fsi MyFirstScript.fsx "zhaowei"
+match fsi.CommandLineArgs with
+    | [| scriptName; word |] ->
+        printfn "running script: %s" scriptName
+        toPigLatin word |> printfn "%A"
+    | _ ->
+        printfn "USAGE: [word]"    
+demo02()
